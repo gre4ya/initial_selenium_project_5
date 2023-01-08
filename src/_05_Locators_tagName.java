5c12212539de8359994dff7dbe4cfe9d54347b5b
Andrii Grechyn <gre4ya@icloud.com>
2023-01-08 13:57:24 -0600

    added new lines of code

--- src/_05_Locators_tagName.java
+++ src/_05_Locators_tagName.java
@@ -1,6 +1,7 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import utils.Driver;
+import utils.TechGlobalUtil;
 import utils.Waiter;
 
 public class _05_Locators_tagName {
@@ -21,11 +22,13 @@ public class _05_Locators_tagName {
             driver.findElement(By.id("card-1")).click();
             Waiter.pause(2);
 
+            // TechGlobalUtil.getFrontendTesting(); can replace lines 13 - 23
+
             System.out.println(driver.findElement(By.id("main_heading")).isDisplayed() ?
                     "\"Locators\" item display validation is PASSED" : "\"Locators\" item display validation is FAILED");
+
             System.out.println(driver.findElement(By.id("main_heading")).getText().equals("Locators") ?
                     "\"Locators\" text validation is PASSED" : "\"Locators\" text validation is FAILED");
-
         }
         catch (Exception e){System.out.println("TEST FAILED due to: " + e.getMessage());}
         finally {Driver.quitDriver();}

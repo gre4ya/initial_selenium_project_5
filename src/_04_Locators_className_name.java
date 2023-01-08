7fbb5207b8e37d4f16437f5d682fa865cfcf98c2
Andrii Grechyn <gre4ya@icloud.com>
2023-01-08 13:23:27 -0600

    added new lines of code

--- src/_04_Locators_className_name.java
+++ src/_04_Locators_className_name.java
@@ -23,6 +23,20 @@ public class _04_Locators_className_name {
 
             driver.findElement(By.id("frontend-option")).click();
             Waiter.pause(2);
+
+            driver.findElement(By.id("card-1")).click();
+            Waiter.pause(2);
+
+
+            System.out.println(driver.findElement(By.id("item_kiwi")).isDisplayed() ?
+                    "\"Kiwi\" item validation is PASSED" : "\"Kiwi\" item validation is FAILED");
+
+            System.out.println(driver.findElement(By.name("item_orange")).isDisplayed() ?
+                   "\"Orange\" item validation is PASSED" : "\"Orange\" item validation is FAILED");
+
+            System.out.println(driver.findElement(By.className("item_grapes")).isDisplayed() ?
+                    "\"Grapes\" item validation is PASSED" : "\"Grapes\" item validation is FAILED");
+
         }
         catch (Exception e){
             System.out.println("TEST FAILED due to: " + e.getMessage());

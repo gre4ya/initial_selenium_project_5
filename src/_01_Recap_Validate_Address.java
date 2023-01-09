1d9d5e554ffb109a4157aaa1c9fdafc3c1ec5817
Andrii Grechyn <gre4ya@icloud.com>
2023-01-09 17:53:20 -0600

    added new lines of code

--- src/_01_Recap_Validate_Address.java
+++ src/_01_Recap_Validate_Address.java
@@ -8,6 +8,14 @@ import java.util.List;
 
 public class _01_Recap_Validate_Address {
     public static void main(String[] args) {
+
+        /*
+        TEST CASE 1
+        Go to https://techglobal-training.netlify.app/
+        Validate the address in the footer is displayed and text is "2860 S River Rd Suite 350, Des Plaines IL 60018"
+        Validate the copyright in the footer is displayed and text is "© 2023 TechGlobal - All Rights Reserved"
+         */
+
         WebDriver driver = Driver.getDriver();
 
         driver.get("https://techglobal-training.netlify.app/");
@@ -25,6 +33,12 @@ public class _01_Recap_Validate_Address {
             if(elements.get(i).getText().equals(actualText[i])){System.out.println("PASSED");}
             else System.out.println("FAILED");
         }
+
+        /*
+        TEST CASE 2
+        Go to https://techglobal-training.netlify.app/
+        Validate both the instagram and facebook items are displayed and enabled
+         */
 
         List<WebElement> footerLinks = driver.findElements(By.xpath("//div[@class='Footer_socials__7h4n1']//a"));
         for (WebElement footerLink : footerLinks) {
